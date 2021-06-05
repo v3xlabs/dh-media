d6f67fbed4e8ab62829379c2441a52683b3a5e92
RonaldColyar <phantexcreations@gmail.com>
2021-06-05 15:00:52 -0500

    init

---
+++ src/config.ts
@@ -0,0 +1,66 @@
+
+const os = require('os');
+
+module.exports = Object.freeze({
+  numWorkers: Object.keys(os.cpus()).length,
+  worker: {
+    logLevel: 'debug',
+    logTags: [
+      'rtp',
+      'srtp',
+      'rtcp',
+    ],
+    rtcMinPort: 40000,
+    rtcMaxPort: 49999
+  },
+  router: {
+    mediaCodecs: [
+      {
+        kind: 'audio',
+        mimeType: 'audio/opus',
+        clockRate: 48000,
+        channels: 2
+      },
+      {
+        kind: 'video',
+        mimeType: 'video/VP8',
+        clockRate: 90000,
+        parameters: {
+          'x-google-start-bitrate': 1000
+        }
+      },
+      {
+        kind: 'video',
+        mimeType: 'video/VP9',
+        clockRate: 90000,
+        parameters: {
+          'profile-id': 2,
+          'x-google-start-bitrate': 1000
+        }
+      },
+      {
+        kind: 'video',
+        mimeType: 'video/H264',
+        clockRate: 90000,
+        parameters: {
+          'packetization-mode': 1,
+          'profile-level-id': '4d0032',
+          'level-asymmetry-allowed': 1,
+          'x-google-start-bitrate': 1000
+        }
+      },
+    ]
+  },
+  webRtcTransport: {
+    listenIps: [ { ip: '0.0.0.0', announcedIp: undefined } ], // TODO: Change announcedIp to your external IP or domain name
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    maxIncomingBitrate: 1500000
+  },
+  plainRtpTransport: {
+    listenIp: { ip: '0.0.0.0', announcedIp: undefined }, // TODO: Change announcedIp to your external IP or domain name
+    rtcpMux: true,
+    comedia: false
+  }
+});
